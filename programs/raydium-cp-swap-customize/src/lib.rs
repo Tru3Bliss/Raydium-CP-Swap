6c7e32bec5d796daa4c7d6e365d72e97f43053e0
Tru3Bliss <monkeydbresin@gmail.com>
2025-03-12 09:40:52 +0000

    feat: instructions::admin

--- programs/raydium-cp-swap-customize/src/lib.rs
+++ programs/raydium-cp-swap-customize/src/lib.rs
@@ -1,9 +1,22 @@
 pub mod curve;
 pub mod error;
+pub mod instructions;
+pub mod states;
 pub mod utils;
 
 use crate::curve::fees::FEE_RATE_DENOMINATOR_VALUE;
 use anchor_lang::prelude::*;
+use instructions::*;
+
+pub const AUTH_SEED: &str = "vault_and_lp_mint_auth_seed";
+
+pub mod admin {
+    use anchor_lang::prelude::declare_id;
+    #[cfg(feature = "devnet")]
+    declare_id!("adMCyoCgfkg7bQiJ9aBJ59H3BXLY3r5LNLfPpQfMzBe");
+    #[cfg(not(feature = "devnet"))]
+    declare_id!("GThUX1Atko4tqhN2NaiTazWSeFWMuiUvfFnyJyUghFMJ");
+}
 
 declare_id!("F4tHwxNDizBtYz4s5rSukS32RY4VA1AYSHjBmywRCcTp");
 
